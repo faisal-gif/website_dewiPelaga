55939897d7ebdb12126133c0bda41ad08289a930
Faisal <fagdera@gmai.com>
2022-07-23 07:15:00 +0700

    make it better53

--- resources/views/layouts/lay.blade.php
+++ resources/views/layouts/lay.blade.php
@@ -60,7 +60,7 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="index3.html" class="brand-link">
+      <a href="/home" class="brand-link">
         
         <span class="brand-text font-weight-light">Dewi Pelaga Website</span>
       </a>
